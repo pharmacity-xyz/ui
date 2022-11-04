04d724198e10ed3813ab4340b344c0a92340e4f8
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-04 11:21:26 +0700

    Update category

--- pages/admin/category/add.tsx
+++ pages/admin/category/add.tsx
@@ -16,7 +16,7 @@ const AddCategory = () => {
         headers: { Authorization: `Bearer ${token}` },
       }
       await addCategoryApi({ categoryName: name }, config)
-      router.push("/admin/category")
+      router.push('/admin/category')
     } catch (error) {
       console.error(error)
     }
@@ -24,32 +24,44 @@ const AddCategory = () => {
 
   return (
     <AdminLayout title="Add Category">
-      <form>
-        <div className="relative z-0 mb-6 w-full group">
-          <input
-            type="text"
-            name="floating_email"
-            id="floating_email"
-            className="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder=" "
-            required
-            onChange={(e) => setName(e.target.value)}
-          />
-          <label
-            htmlFor="floating_email"
-            className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
-          >
-            Name
-          </label>
-        </div>
-        <button
-          type="button"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          onClick={() => handleAddCategory()}
-        >
-          Add
-        </button>
-      </form>
+      <div className="border p-2">
+        <h1 className="text-2xl text-center m-2">Add Category</h1>
+        <form>
+          <div className="relative z-0 mb-6 w-full group">
+            <input
+              type="text"
+              name="floating_email"
+              id="floating_email"
+              className="block py-2.5 px-0 w-full bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+              placeholder=" "
+              required
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label
+              htmlFor="floating_email"
+              className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"
+            >
+              Name
+            </label>
+          </div>
+          <div className="flex justify-evenly">
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={() => handleAddCategory()}
+            >
+              Add
+            </button>
+            <button
+              type="button"
+              className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+              onClick={() => router.push('/admin/category')}
+            >
+              Cancel
+            </button>
+          </div>
+        </form>
+      </div>
     </AdminLayout>
   )
 }
