7ba787448db08747d8708b93a71c61505a0426ce
Aoi1011 <ht.so.ik0102@gmail.com>
2022-11-04 16:32:23 +0700

    Update layout

--- components/Layout/ShopLayout.tsx
+++ components/Layout/ShopLayout.tsx
@@ -1,125 +1,87 @@
 import React, { ReactNode } from 'react'
 import Link from 'next/link'
+import { IReturnGetCategories } from '../../services/category/types'
 
 type Props = {
   children: ReactNode
+  categories: Array<IReturnGetCategories>
+  setSearchWord: (searchWord: string) => void
+  searchProducts: () => void
 }
 
-const ShopLayout = ({ children }: Props) => {
+const ShopLayout = ({
+  children,
+  categories,
+  setSearchWord,
+  searchProducts,
+}: Props) => {
   return (
-    <div className="flex flex-no-wrap h-screen">
-      <div className="w-64 absolute sm:relative bg-gray-800 shadow h-full flex-col justify-between hidden sm:flex">
+    <div className="flex w-full">
+      <div className="w-1/3 absolute bg-[#52BA2D] shadow flex-col justify-between hidden sm:flex">
         <div className="px-8">
+          {/* <div className="flex items-center" id="store-nav-content">
+            <input
+              type="text"
+              autoFocus={false}
+              placeholder="Search"
+              className="text-black bg-[#52BA2D] border-b-2 border-black placeholder-slate-700"
+              onChange={(e) => setSearchWord(e.target.value)}
+            />
+            <svg
+              className="fill-current hover:text-black cursor-pointer text-black"
+              onClick={() => searchProducts()}
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
+            </svg>
+          </div> */}
+          <div className="relative mt-10 rounded-md shadow-sm">
+            <div
+              className="flex items-center justify-between gap-2"
+              id="store-nav-content"
+            >
+              <input
+                type="text"
+                name="price"
+                id="price"
+                className="block w-full rounded-md border-gray-300 pl-2 py-2 focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                placeholder="Search"
+              />
+              <svg
+                className="fill-current hover:text-black cursor-pointer text-black"
+                onClick={() => searchProducts()}
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+              >
+                <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
+              </svg>
+            </div>
+          </div>
           <div className="h-16 w-full flex items-center">
             <Link href="/">
               <span className="font-semibold text-xl tracking-tight cursor-pointer text-white">
-                MedicalEquipmentShop
+                Categories
               </span>
             </Link>
           </div>
-          <ul className="mt-12">
-            <Link href="/admin/user">
-              <li className="flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6">
-                <div className="flex items-center">
-                  <span className="text-sm  ml-2">User</span>
-                </div>
-              </li>
-            </Link>
-            <Link href="/admin/category">
-              <li className="flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6">
-                <div className="flex items-center">
-                  <span className="text-sm  ml-2">Category</span>
-                </div>
-              </li>
-            </Link>
-            <Link href="/admin/product">
-              <li className="flex w-full justify-between text-gray-300 hover:text-gray-500 cursor-pointer items-center mb-6">
-                <div className="flex items-center">
-                  <span className="text-sm  ml-2">Product</span>
-                </div>
-              </li>
-            </Link>
-          </ul>
-        </div>
-        <div className="px-8 border-t border-gray-700">
-          <ul className="w-full flex items-center justify-between bg-gray-800">
-            <li className="cursor-pointer text-white pt-5 pb-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-bell"
-                width={20}
-                height={20}
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
+          <div className="gap-4">
+            {categories.map((category) => (
+              <button
+                key={category.categoryId}
+                className="mx-3 px-3 h-8 rounded-md border-black"
               >
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <path d="M10 5a2 2 0 0 1 4 0a7 7 0 0 1 4 6v3a4 4 0 0 0 2 3h-16a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6" />
-                <path d="M9 17v1a3 3 0 0 0 6 0v-1" />
-              </svg>
-            </li>
-            <li className="cursor-pointer text-white pt-5 pb-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-messages"
-                width={20}
-                height={20}
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <path d="M21 14l-3 -3h-7a1 1 0 0 1 -1 -1v-6a1 1 0 0 1 1 -1h9a1 1 0 0 1 1 1v10" />
-                <path d="M14 15v2a1 1 0 0 1 -1 1h-7l-3 3v-10a1 1 0 0 1 1 -1h2" />
-              </svg>
-            </li>
-            <li className="cursor-pointer text-white pt-5 pb-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-settings"
-                width={20}
-                height={20}
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <path d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-                <circle cx={12} cy={12} r={3} />
-              </svg>
-            </li>
-            <li className="cursor-pointer text-white pt-5 pb-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="icon icon-tabler icon-tabler-archive"
-                width={20}
-                height={20}
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path stroke="none" d="M0 0h24v24H0z" />
-                <rect x={3} y={4} width={18} height={4} rx={2} />
-                <path d="M5 8v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-10" />
-                <line x1={10} y1={12} x2={14} y2={12} />
-              </svg>
-            </li>
-          </ul>
+                {category.name}
+              </button>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="container mx-auto py-10 h-64 md:w-4/5 w-11/12 px-6">
+      <div className="h-64 w-2/3 px-6">
         {children}
       </div>
     </div>
