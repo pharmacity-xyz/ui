88567762b5812e5194c8d1b0a35860ac21d8a138
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-03 08:48:52 +0700

    absolute path

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -2,17 +2,17 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 
-import Layout from '../components/Layout'
-import { getAllCategoriesApi } from '../services/category/categoryServices'
-import { IReturnGetCategories } from '../services/category/types'
-import { IReturnProducts } from '../services/product/types'
+import Layout from 'components/Layout'
+import { getAllCategoriesApi } from 'services/category/categoryServices'
+import { IReturnGetCategories } from 'services/category/types'
+import { IReturnProducts } from 'services/product/types'
 import {
   getAllProductsApi,
   getProductsByCategoryApi,
   searchProductsApi,
-} from '../services/product/productServices'
-import Spinner from '../components/Spinner'
-import ShopLayout from '../components/Layout/ShopLayout'
+} from 'services/product/productServices'
+import Spinner from 'components/Spinner'
+import ShopLayout from 'components/Layout/ShopLayout'
 
 const Shop = () => {
   const [categories, setCategories] = useState<Array<IReturnGetCategories>>([])
@@ -120,66 +120,64 @@ const Shop = () => {
   }, [])
 
   return (
-    <>
-      <Layout title="Shop">
-        <div className="">
-          <section className="bg-white">
-            <ShopLayout
-              categories={categories}
-              setSearchWord={setSearchWord}
-              searchProducts={searchProducts}
-              searchProductsByCategory={searchProductsByCategory}
-              handleSelectSort={handleSelectSort}
-            >
-              {loading ? (
-                <div className="w-full h-44 flex justify-center items-center">
-                  <Spinner />
-                </div>
-              ) : (
-                <>
-                  {products.length > 0 ? (
-                    <>
-                      {products.map((product) => (
-                        <Link
-                          href={{
-                            pathname: '/product/[id]',
-                            query: { id: product.productId },
-                          }}
-                          key={product.productId}
-                        >
-                          <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col cursor-pointer">
-                            <>
-                              {product.imageUrl && (
-                                <Image
-                                  src={product.imageUrl}
-                                  alt={product.productName}
-                                  width={300}
-                                  height={300}
-                                />
-                              )}
-                              <div className="pt-3 flex items-center justify-between">
-                                <p className="w-full text-ellipsis whitespace-nowrap overflow-hidden">
-                                  {product.productName}
-                                </p>
-                              </div>
-                              <p className="pt-1 text-gray-900">
-                                $ {product.price}
+    <Layout title="Shop">
+      <div className="">
+        <section className="bg-white">
+          <ShopLayout
+            categories={categories}
+            setSearchWord={setSearchWord}
+            searchProducts={searchProducts}
+            searchProductsByCategory={searchProductsByCategory}
+            handleSelectSort={handleSelectSort}
+          >
+            {loading ? (
+              <div className="w-full h-44 flex justify-center items-center">
+                <Spinner />
+              </div>
+            ) : (
+              <>
+                {products.length > 0 ? (
+                  <>
+                    {products.map((product) => (
+                      <Link
+                        href={{
+                          pathname: '/product/[id]',
+                          query: { id: product.productId },
+                        }}
+                        key={product.productId}
+                      >
+                        <div className="w-full md:w-1/3 xl:w-1/4 p-6 flex flex-col cursor-pointer">
+                          <>
+                            {product.imageUrl && (
+                              <Image
+                                src={product.imageUrl}
+                                alt={product.productName}
+                                width={300}
+                                height={300}
+                              />
+                            )}
+                            <div className="pt-3 flex items-center justify-between">
+                              <p className="w-full text-ellipsis whitespace-nowrap overflow-hidden">
+                                {product.productName}
                               </p>
-                            </>
-                          </div>
-                        </Link>
-                      ))}
-                    </>
-                  ) : (
-                    <h1>Can not find...</h1>
-                  )}
-                </>
-              )}
-            </ShopLayout>
-          </section>
-        </div>
-      </Layout>
-    </>
+                            </div>
+                            <p className="pt-1 text-gray-900">
+                              $ {product.price}
+                            </p>
+                          </>
+                        </div>
+                      </Link>
+                    ))}
+                  </>
+                ) : (
+                  <h1>Can not find...</h1>
+                )}
+              </>
+            )}
+          </ShopLayout>
+        </section>
+      </div>
+    </Layout>
   )
 }
 
