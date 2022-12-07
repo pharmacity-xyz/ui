f5d45ed35caf1268f3ce6332e5167978a543a00a
Aoi1011 <ht.so.ik0102@gmail.com>
2022-12-07 13:53:25 +0700

    Change the type of button in register page

--- pages/register.tsx
+++ pages/register.tsx
@@ -72,7 +72,12 @@ const Register: NextPage = () => {
             />
           </div>
           <div className="xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 mb-12 md:mb-0">
-            <form>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                handleValidateForm()
+              }}
+            >
               <div className="mb-6">
                 <input
                   type="email"
@@ -222,9 +227,8 @@ const Register: NextPage = () => {
               </div>
               <div className="text-center lg:text-left">
                 <button
-                  type="button"
+                  type="submit"
                   className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
-                  onClick={() => handleValidateForm()}
                 >
                   Register
                 </button>
